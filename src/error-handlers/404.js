07eeca3ff3c8b4861482fb8e4e3fe059e30b4da4
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-02-16 20:41:04 -0800

    all features complete except error handling and testing requirements

---
+++ src/error-handlers/404.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function (err, request, response, next) {
+  const error = err.message ? err.message : err;
+
+  const errorObject = {
+    status: 404,
+    message: error,
+  }
+
+  response.status(errorObject.status).json(errorObject);
+}
